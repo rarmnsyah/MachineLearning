ba06caceb31137be9eb72644882a0f75f8859662
Risky Armansyah <rarmnsyah787@gmail.com>
2022-04-07 08:57:04 +0700

    committing some freak change

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-import numpy as np
+randlist = [2,3,12,4]
 
-path = 'C:/Users/LENOVO/Programming/Python/Machine Learning'
-
-file1 = open(f'C:/Users/LENOVO/Programming/Python/MachineLearning/model/modelNNReadIris.txt', 'r')
-database = file1.readlines()
-sample_data = []
-
-for i in database:
-    sample_data.append(i.split(','))
-
-for i in sample_data:
-    print(len(i))
+print(randlist.pop(-1))
+print(randlist)
